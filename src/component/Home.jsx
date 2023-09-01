4a484ffcd419b821d91264ada3afa77ff7756d69
SHRUTIKA WANKHADE <94029908+Shrutika99@users.noreply.github.com>
2023-09-01 23:30:13 +0530

    Add files via upload

---
+++ src/component/Home.jsx
@@ -0,0 +1,67 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import './home1.css'
+
+function Home() {
+  const [jobs, setJobs] = useState([]);
+  const [page, setPage] = useState(1);
+
+  useEffect(() => {
+    const fetchJobs = async () => {
+      try {
+        const response = await axios.get(
+          `https://hacker-news.firebaseio.com/v0/jobstories.json`
+        );
+
+        const jobIds = response.data.slice((page - 1) * 3, page * 3);
+
+        const jobPromises = jobIds.map(async (jobId) => {
+          const jobResponse = await axios.get(
+            `https://hacker-news.firebaseio.com/v0/item/${jobId}.json`
+          );
+          return jobResponse.data;
+        });
+
+        const jobData = await Promise.all(jobPromises);
+        setJobs((prevJobs) => [...prevJobs, ...jobData]);
+      } catch (error) {
+        console.error('Error fetching job data:', error);
+      }
+    };
+
+    fetchJobs();
+  }, [page]);
+
+
+
+  return (
+    <div>
+        
+        <div className="topnav">
+  <a className="active" href="#home">Home</a>
+  <a href="#news">News</a>
+  <a href="#contact">Contact</a>
+  <a href="#about">About</a>
+</div>
+        
+        <div className='header'>
+            <h1>HACKER NEWS JOBS BOARD</h1>
+        </div>
+      {jobs.map((job) => (
+        <div key={job.id} className='container'>
+          {job.url ? (
+            <a href={job.url} target="_blank" rel="noopener noreferrer">
+              {job.title}
+            </a>
+          ) : (
+            <span>{job.title}</span>
+          )}
+          <p>Posted by {job.by} on {new Date(job.time * 1000).toLocaleDateString()}</p>
+        </div>
+      ))}
+      <button onClick={() => setPage(page + 1)}>Load More</button>
+    </div>
+  );
+}
+
+export default Home;
